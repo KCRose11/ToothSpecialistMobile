8a25b70b58d2ee131cf8855ceeba467ed02f5b93
KC Rose Calaguio <kcab.calaguio.up@phinmaed.com>
2023-10-10 07:00:02 +0800

    masakit na ulo ko

---
+++ app/src/main/java/com/example/toothspecialistapp/Book_an_Appointment_Activity.java
@@ -0,0 +1,14 @@
+package com.example.toothspecialistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Book_an_Appointment_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_an_appointment);
+    }
+}
